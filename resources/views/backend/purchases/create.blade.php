2854fcae17545cc72c8594d506452f4d2eda6133
Tofayel Ahmmad Topu <topu.steadfast@gmail.com>
2025-03-05 11:19:23 +0600

    static image file link removed to remove console error

--- resources/views/backend/purchases/create.blade.php
+++ resources/views/backend/purchases/create.blade.php
@@ -255,7 +255,7 @@
                     <td>
                         <div class="product-quantity">
                             <span class="quantity-btn" >+<i data-feather="plus-circle" class="plus-circle"></i></span>
-                            <input type="text" id="qty${id}" name="qty[]" data-price="${purchase_price}" class="quntity-input" value="1" data-id="${id}" >
+                            <input type="number" id="qty${id}" name="qty[]" data-price="${purchase_price}" class="quntity-input" value="1" data-id="${id}" >
                             <span class="quantity-btn" ><i data-feather="minus-circle" class="feather-search"></i></span>
                         </div>
                     </td>
